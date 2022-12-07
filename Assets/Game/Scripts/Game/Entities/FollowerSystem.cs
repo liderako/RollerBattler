30b13d53a6d7ef1554c30e1bd70481086d17d6f1
liderako <trotillostudio@gmail.com>
2022-12-07 16:24:02 +0200

    update ragdoll

---
+++ Assets/Game/Scripts/Game/Entities/FollowerSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Scripts.Game
+{
+    public class FollowerSystem : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        private Vector3 offset;
+        
+        private void Start()
+        {
+            offset = transform.localPosition - target.localPosition;
+        }
+
+        private void FixedUpdate()
+        {
+            Follow();
+        }
+
+        private void Follow()
+        {
+            transform.localPosition = new Vector3(target.localPosition.x + offset.x, target.localPosition.y, target.localPosition.z + offset.z);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Game/Entities/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Game
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        private Vector3 direction;
+        [SerializeField] private List<Rigidbody> rigidbody;
+        private InputSystem inputSystem;
+        private int amountBones;
+        // private float deltaSpeed;
+
+        private void Awake()
+        {
+            direction = new Vector3();
+            amountBones = rigidbody.Count;
+            // deltaSpeed = speed * Time.fixedDeltaTime;
+        }
+
+        private void Start()
+        {
+            inputSystem = GetComponent<InputSystem>();
+            inputSystem.OnDirectionInputActionPlayer += UpdateDir;
+        }
+
+        private void OnDestroy()
+        {
+            inputSystem.OnDirectionInputActionPlayer -= UpdateDir;
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            for (int i = 0; i < amountBones; i++)
+            {
+                rigidbody[i].AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.Force);
+            }
+        }
+
+        [SerializeField] private Transform p; 
+        private void UpdateDir(float x, float  y)
+        {
+            direction.x = x;
+            direction.z = y;
+            if (x == 0 && y == 0)
+            {
+                return;
+            }
+            float angle = 90 - Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+            p.rotation = Quaternion.Euler(0, angle, 0);
+        }
+    }
+}
