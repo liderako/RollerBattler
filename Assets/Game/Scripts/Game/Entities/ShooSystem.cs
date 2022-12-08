97a940c2110114c8f4358b55081d624fd77dad22
liderako <trotillostudio@gmail.com>
2022-12-08 13:57:17 +0200

    backhuo shot system

--- Assets/Game/Scripts/Game/Entities/ShooSystem.cs
+++ Assets/Game/Scripts/Game/Entities/ShooSystem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Entities
@@ -6,10 +7,19 @@ namespace Game.Entities
     public class ShooSystem : MonoBehaviour
     {
         [SerializeField] private Transform spriteRight;
-        [SerializeField] private float d;
+        [SerializeField] private float maxScale;
         [SerializeField] private float stepP = 0.3f;
         [SerializeField] private float stepS = 0.5f;
         [SerializeField] private float stepSX = 0.1f;
+
+        [SerializeField] private List<Transform> enemies;
+        
+        private float originScale;
+
+        private void Awake()
+        {
+            originScale = maxScale;
+        }
 
         private void Start()
         {
@@ -17,15 +27,67 @@ namespace Game.Entities
 
         private void Update()
         {
-            if (spriteRight.localScale.y < d)
+            if (CheckEnemyInRange())
             {
-                spriteRight.transform.localPosition = Vector3.Lerp(spriteRight.transform.localPosition,
-                    new Vector3(spriteRight.transform.localPosition.x + stepP, spriteRight.transform.localPosition.y,
-                        spriteRight.transform.localPosition.z), 1f);
-                spriteRight.transform.localScale = Vector3.Lerp(spriteRight.transform.localScale,
-                    new Vector3(spriteRight.transform.localScale.x + stepSX, spriteRight.transform.localScale.y + stepS,
-                        spriteRight.transform.localScale.z), 1f);
+                RenderVision(true);
             }
+            else
+            {
+                RenderVision(false);   
+            }
+        }
+
+        [SerializeField] private Transform owner;
+        [SerializeField] private float yStart;
+        [SerializeField] private float step = 5.0f;
+        [SerializeField] private int amountRay = 5;
+        [SerializeField] private float rangeVision = 30;
+        private bool CheckEnemyInRange()
+        {
+            float yStartTMP = yStart;
+            RaycastHit hit;
+
+            for (int i = 0; i < amountRay; i++)
+            {
+                Vector3 dir = Quaternion.Euler(0, yStartTMP, 0) * (owner.right * rangeVision);
+                Debug.DrawRay(owner.position, dir, Color.yellow);
+                if (Physics.Raycast(owner.position, dir, out hit, rangeVision))
+                {
+                    // if (hit.collider.TryGetComponent())
+                    Debug.DrawRay(owner.position, dir, Color.black);
+                    return true;
+                }
+                yStartTMP += step;
+            }
+            return false;
+        }
+
+        private void RenderVision(bool state)
+        {
+            if (state && spriteRight.localScale.y < maxScale)
+            {
+                ChangeLocalPosition(stepP);
+                ChangeScale(stepSX, stepS);
+            }
+            else if (!state && spriteRight.localScale.y > originScale)
+            {
+                ChangeLocalPosition(-stepP);
+                ChangeScale(-stepSX, -stepS);
+            }
+        }
+
+        private void ChangeLocalPosition(float tmpX)
+        {
+            spriteRight.transform.localPosition = Vector3.Lerp(spriteRight.transform.localPosition,
+                new Vector3(spriteRight.transform.localPosition.x + tmpX, spriteRight.transform.localPosition.y,
+                    spriteRight.transform.localPosition.z), 1f);
+        }
+
+        private void ChangeScale(float tmpX, float tmpY)
+        {
+            spriteRight.transform.localScale = Vector3.Lerp(spriteRight.transform.localScale,
+                new Vector3(spriteRight.transform.localScale.x + tmpX, spriteRight.transform.localScale.y + tmpY,
+                    spriteRight.transform.localScale.z), 1f);
         }
     }
 }
